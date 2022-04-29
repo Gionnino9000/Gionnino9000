ebf6be8e76ab8e5a83f753e01cf70f23f17195aa
Michele Righi <righi.michy@gmail.com>
2022-04-30 00:04:47 +0200

    update
    created empty classes for the specific heuristics

---
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/gionnino9000/heuristics/BlackHeuristics.java
@@ -0,0 +1,9 @@
+package it.unibo.ai.didattica.competition.tablut.gionnino9000.heuristics;
+
+import it.unibo.ai.didattica.competition.tablut.domain.State;
+
+public class BlackHeuristics extends Heuristics{
+    public BlackHeuristics(State state) {
+        super(state);
+    }
+}
---
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/gionnino9000/heuristics/WhiteHeuristics.java
@@ -0,0 +1,9 @@
+package it.unibo.ai.didattica.competition.tablut.gionnino9000.heuristics;
+
+import it.unibo.ai.didattica.competition.tablut.domain.State;
+
+public class WhiteHeuristics extends Heuristics{
+    public WhiteHeuristics(State state) {
+        super(state);
+    }
+}
